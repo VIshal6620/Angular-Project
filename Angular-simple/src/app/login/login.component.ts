6a9c22af9dfd2e3db9fa93feec2a1863c1a64c6b
VIshal6620 <vishalvish7099@gmail.com>
2025-05-05 15:05:49 +0530

    05-05-2025

---
+++ Angular-simple/src/app/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  
+  form: any = {
+    data: {},
+    message: '',
+  }
+
+  constructor(private httpClient:HttpClient , public router:Router) {
+
+  }
+
+  signIn() {
+    this.httpClient.post('http://localhost:8000/ORSAPI/login/',this.form.data).subscribe((res:any)=>{
+      console.log('res=>',res)
+      this.form.message='';
+
+      if(res.result.message){
+        this.form.message=res.result.message;
+      }
+
+      if(res.result.data !=null){
+        localStorage.setItem('firstName',res.result.data.firstName)
+        this.router.navigateByUrl('welcome');
+      }
+      console.log('data',res.result.data)
+      console.log('LocalStorage:', localStorage.getItem('firstName'));
+
+    })
+  }
+
+  signUp() {
+    this.router.navigateByUrl('signup')
+  }
+
+}
